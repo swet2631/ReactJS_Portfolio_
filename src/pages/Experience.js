b00b9be295ddee9601de9fb4c24f8e928a713bad
Your Name <swet2631@gmail.com>
2022-06-23 21:31:22 +0530

    all done

---
+++ src/pages/Experience.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { Card } from "react-bootstrap";
+
+function Experience() {
+    return (
+        <React.Fragment>
+            <div className="min-h-screen" style={{background:"black"}}>
+                <div className="container py-5">
+                    <h1 className="text-center mb-5" style={{color:"white"}}>Experience</h1>
+                    <div className="row">
+                        <div className="col-md-6 offset-md-3">
+                        <div className="card mb-3">
+                            <Card className="text-center">
+                            <Card.Header style={{background:"#286f6f"}}><b style={{color:"white"}}>Coding Gujju</b></Card.Header>
+                                <Card.Body>
+                                    <Card.Title>Full Stack Developer</Card.Title>
+                                        <Card.Text>
+                                                I was an Intern at Codding Gujju.
+                                        </Card.Text>
+                                    </Card.Body>
+                                 <Card.Footer className="text-muted">6-JUNE T0 24-JUNE</Card.Footer>
+                            </Card> 
+                        </div>         
+                    </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-md-6 offset-md-3">
+                            <div className="card mb-3">
+                            <Card className="text-center">
+                            <Card.Header style={{background:"#286f6f"}}><b style={{color:"white"}}>Creart</b></Card.Header>
+                                <Card.Body>
+                                    <Card.Title>Full Stack Developer</Card.Title>
+                                        <Card.Text>
+                                                I was an Intern at Creart.
+                                        </Card.Text>
+                                    </Card.Body>
+                                 <Card.Footer className="text-muted">MAY-2021 T0 JUNE-2021</Card.Footer>
+                            </Card> 
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </React.Fragment>
+    );
+}
+
+export default Experience;
